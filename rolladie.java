f3579c121192d68cbf081769219a18c6995f88aa
David Bankole Abel <ballack_davidabel@yahoo.com>
2022-06-15 19:11:03 -0400

    Create rolladie.java

---
+++ rolladie.java
@@ -0,0 +1,21 @@
+package rolladie;
+
+import java.util.Random;
+
+public class RollADie {
+
+
+    public static void main(String [] args) {
+
+        Random r = new Random ();
+
+        int result = 0;
+
+        for (int i = 0; i < 6; i++) {
+          int result = r.nextInt(6);
+            result++;
+
+            System.out.println("you rolled a " + result);
+        }
+    }
+}
